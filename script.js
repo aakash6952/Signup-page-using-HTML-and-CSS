5000ae833c22cddfc1a7cc868728280b2bf0497f
Aakash Kashyap <88430118+aakash6952@users.noreply.github.com>
2024-02-03 16:14:54 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getInfo(){
 const Full_name =document.getElementById("fullname").value;
 const  email =document.getElementById("mail").value;
-const password = document.getElementById("password").value;
+const pasword = document.getElementById("Password").value;
 // const document.getElementById("")
 
 document.write("Yor filled the following details ! <br>")
 document.writeln("Your Name: "+ Full_name+"<br>");
 document.writeln("Your email: "+ email+"<br>");
-document.writeln("Your Name: "+ password+"<br>");
+document.writeln("Your Name: "+ pasword+"<br>");
 
 }
